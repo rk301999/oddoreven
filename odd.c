16182a86b8c7bf44f28a5c7c10ce19021f3f3ee1
ritesh kumar <42288787+rk301999@users.noreply.github.com>
2019-10-02 19:15:46 +0530

    completed

--- odd.c
+++ odd.c
@@ -2,12 +2,13 @@
 #include<stdio.h>
 int main(){
   int n;
-
   scanf("%d",&n);
   if(n%2==0){
     printf("this is even no");
   }
   else{
-    printf("this is even no");
+    printf("this is odd no");
   }
+  return 0;
 }
+//this program  doesnt deal with 0:/
